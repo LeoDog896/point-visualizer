abec9b75f3054b7cf759e905ec352531fe74cd6a
Tristan F <LeoDog896@hotmail.com>
2023-04-29 18:14:16 -0400

    feat: resizability

--- index.ts
+++ index.ts
@@ -68,3 +68,9 @@ function animate() {
   controls.update();
 }
 animate();
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
